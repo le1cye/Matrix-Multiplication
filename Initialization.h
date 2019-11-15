b28c698639faa65a5fb7cb5d07d164556f36bd6d
le1cye <57780291+le1cye@users.noreply.github.com>
2019-11-15 10:02:06 +0800

    new re

---
+++ Initialization.h
@@ -0,0 +1,7 @@
+
+#ifndef _INITIALIZATION_
+#define _INITIALIZATION_
+void InitializeMatrix(double **Matrix,int m,int n,double InitialValue);
+
+void InitializeArray(double *Array,int m,double InitialValue);
+#endif
---
+++ PrintToFile.h
@@ -0,0 +1,6 @@
+#ifndef _PRINTTOFILE
+#define _PRINTTOFILE
+void PrintToFile(double **Matrix,int mm,int nn,double *CoordX,double *CoordY,double *CoordZ,int i,char *Directory,char *ArrayName,double var1);
+void PrintMatrix(double **Matrix,char *Directory,char *MatrixName,int m,int n);
+
+#endif
